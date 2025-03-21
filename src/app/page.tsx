ebad2fb0e8494ff75a7e4bd6375af89fc27f835a
Fouad <fouadezari@gmail.com>
2025-03-21 02:16:55 +0100

    Feat: Ajout de la section AgendaEvents avec des cartes d'événements, intégration du composant EventCard, et mise à jour de la page d'accueil pour inclure la section de carte Gironde et les événements. Amélioration de la structure et des styles des sections.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { HeroSelector } from "@/sections/Hero"
+import AgendaEvents from "@/sections/agenda/event-section"
+import MapGirondeSection from "@/sections/map/map-gironde"
 import StatisticSection from "@/sections/stat/statistic"
 
 export default function Home() {
   return (
     <main className="flex flex-1 flex-col items-center justify-center gap-4">
-      {/* <h1 className="gradient-primary text-6xl font-bold">RPDAD</h1> */}
       <HeroSelector variant="img-grid" />
+      <MapGirondeSection />
       <StatisticSection />
+      <AgendaEvents />
     </main>
   )
 }
