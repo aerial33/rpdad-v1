11b75f84449520040db5dfdf61f8549b4f597339
Fouad <fouadezari@gmail.com>
2025-03-21 11:01:57 +0100

    Refactor: Renommage du composant ServicesMembres en ServiceMembrePage pour une meilleure clarté, et amélioration de la récupération du nom du service en intégrant le slug dans le fallback.

--- src/app/services-membres/[...slug]/page.tsx
+++ src/app/services-membres/[...slug]/page.tsx
@@ -1,13 +1,17 @@
 import BlogSection from "@/components/blog-content/content-blog"
 import { getServiceBySlug } from "@/lib/services"
 
-const ServicesMembres = async ({ params }: { params: { slug: string } }) => {
-  const service = getServiceBySlug(params.slug)?.name || `CCAS/CIAS `
+export default async function ServiceMembrePage({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const service = getServiceBySlug(params.slug)
+  const serviceName = service?.name || `CCAS/CIAS ${params.slug}`
+
   return (
-    <div>
-      <BlogSection service={service} />
+    <div className="container mx-auto py-8">
+      <BlogSection service={serviceName} />
     </div>
   )
 }
-
-export default ServicesMembres
